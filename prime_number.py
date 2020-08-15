623cf912be8f9ab29e8cac45295889ef534612ad
GHz199 <67038893+GHz199@users.noreply.github.com>
2020-08-15 23:10:37 +0200

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -12,5 +12,5 @@ def nombrepremier(n:int,l:int):
             #print(n)   #uncomment to get console output
             file.write("%d\n" %n)   #comment to speed up
         n=n+1
-nombrepremier(2,5000000)
+nombrepremier(2,5000000)    #5000000 is default limit, change it to get higher range
 file.close()    #comment to speed up
