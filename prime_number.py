e9cdb615a2b6e3cabe90063f4415fb84f3b2d483
GHz199 <67038893+GHz199@users.noreply.github.com>
2020-08-15 23:07:05 +0200

    Add files via upload

---
+++ prime_number.py
@@ -0,0 +1,16 @@
+# CODED BY GHz199
+import math
+from numba import jit
+file = open("prime_number.txt","w")     #comment to speed up
+@jit(parallel = True, fastmath = True)
+def nombrepremier(n:int,l:int):
+    while n<=l:
+        i=2
+        while i*i<=n and n%i!=0:
+            i=i+1
+        if i*i>n and n>1:
+            #print(n)   #uncomment to get console output
+            file.write("%d\n" %n)   #comment to speed up
+        n=n+1
+nombrepremier(2,5000000)
+file.close()    #comment to speed up
